8e54861646567d534f1fd47fac95df81aa87a563
Michael <moreno.m2698@gmail.com>
2023-08-22 11:51:22 -0400

    Corrected port number

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.listen(5000, () => console.log("API is running on port 5001"));
+app.get('/', (req, res) => res.json('The API is running.'));
 
-app.get('/', (req, res) => res.json('The API is running.'))
+app.listen(5001, () => console.log("API is running on port 5001"));
+
